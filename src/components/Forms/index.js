958d4dcb4f057a66b4e9e09f8badee13aa88303e
gbrllcavichion <cavichionmichel@gmail.com>
2024-01-20 16:49:32 -0300

    ✨ Creating roles in forms and button

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,14 +1,35 @@
 import Field from '../Field'
+import Dropdown from '../Dropdown'
+import Button from '../Button'
 import './Forms.css'
 
 const Forms = () => {
+
+    const roles = [
+      'In Game Leader',
+      'Entry Fragger',
+      'Flank Watcher',
+      'Support Roles',
+      'Anchor',
+      'Roamers'
+    ]
+
+    const whenSave = (events) => {
+        events.preventDefault()
+        console.log('Form has been submitted')
+    }
+
     return (
         <section className='forms'>
-            <form>
+            <form onSubmit={whenSave}>
                 <h2>Fill in the fields to create your player card</h2>
-                <Field label="Nickname" placeholder="Enter your nickname"></Field>
-                <Field label="Operator" placeholder="Enter your operator"></Field>
-                <Field label="Image" placeholder="Enter the image address"></Field>
+                <Field mandatory={true} label="Nickname" placeholder="Enter your nickname"></Field>
+                <Field mandatory={true} label="Operator" placeholder="Enter your operator"></Field>
+                <Field mandatory={true} label="Image" placeholder="Enter the image address"></Field>
+                <Dropdown mandatory={true} label="Role" itens={roles}/>
+                <Button>
+                    Submit
+                </Button>
             </form>
         </section>
     )
