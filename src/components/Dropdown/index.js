596ff000eb268708fbe911b27909a40275c420a6
gbrllcavichion <cavichionmichel@gmail.com>
2024-01-23 16:45:19 -0300

    🐛 Dropdown issue fixed!

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -6,7 +6,7 @@ const Dropdown = (props) => {
             <label>
                 {props.label}
             </label>
-            <select onChange={events => props.whenChanged(events.target.value)} required={props.required} value={props.values}>
+            <select onChange={events => props.whenChanged(events.target.value)} required={props.required} value={props.value}>
                 <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item} </option>)}
             </select>
